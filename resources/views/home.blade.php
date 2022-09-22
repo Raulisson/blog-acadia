75f5b2387cc5f17b0bfd6cd0156198d851e87fbc
Raulisson <raulissonvalbuquerque@gmail.com>
2022-09-22 00:07:55 -0300

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog | Jornal de Acadia | Principais Notícias</title>
+
+    {{-- CSS --}}
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/homestyle.css') }}">
+
+</head>
+<body>
+    <div class="main-container">
+        <div class="title-container">
+            <img src="{{ asset('img/ulamog.jpg') }}" class="title-image">
+            <h1> Jornal de Acadia </h1>
+            <p> A maior fonte de notícias de todo o plano </p>
+        </div>
+    </div>
+</body>
+</html>
