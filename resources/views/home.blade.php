d38a18fd1b0ed8942f66faf4fd852570628e3dee
Raulisson <raulissonvalbuquerque@gmail.com>
2022-09-22 09:21:13 -0300

    icon, header and logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,18 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog | Jornal de Acadia | Principais Notícias</title>
+    <title>Blog | Torres de Acadia |</title>
 
     {{-- CSS --}}
     <link rel="stylesheet" type="text/css" href="{{ asset('css/homestyle.css') }}">
-
+    {{-- Fontawesome --}}
+    <script src="https://kit.fontawesome.com/92e90f8568.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="main-container">
         <div class="title-container">
-            <img src="{{ asset('img/ulamog.jpg') }}" class="title-image">
-            <h1> Jornal de Acadia </h1>
-            <p> A maior fonte de notícias de todo o plano </p>
+            <i class="fa-thin fa-eye-slash"></i>
+            <img src="{{ asset('img/logo.png') }}" class="title-image">
+            <i class="fa-solid fa-circle-user"></i>
+        </div>
+        <div class="header">
+            <h1>Home</h1>
+            <i class="fa-solid fa-hat-wizard"></i>
+            <h1>Principais Notícias</h1>
+            <i class="fa-solid fa-hat-wizard"></i>
+            <h1>Quem Somos</h1>
+            <i class="fa-solid fa-hat-wizard"></i>
+            <h1>Contato</h1>
         </div>
     </div>
 </body>
